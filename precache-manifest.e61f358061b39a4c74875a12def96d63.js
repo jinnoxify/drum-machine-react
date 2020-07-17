9ac19e7a9392fe581553486af8ff66769f2c2bfa
jinnoxify <oscarpereznavarro1997@gmail.com>
2020-07-17 18:30:36 -0500

    Updates

---
+++ precache-manifest.e61f358061b39a4c74875a12def96d63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ec3bdeffbec6f6140cfc281918b19d",
+    "url": "/drum-machine-react/index.html"
+  },
+  {
+    "revision": "b6c1d783a3b2cc2a2412",
+    "url": "/drum-machine-react/static/css/main.5becfe8c.chunk.css"
+  },
+  {
+    "revision": "9040b4fbea39b8677925",
+    "url": "/drum-machine-react/static/js/2.1ffe4ef7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine-react/static/js/2.1ffe4ef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c1d783a3b2cc2a2412",
+    "url": "/drum-machine-react/static/js/main.131f8aba.chunk.js"
+  },
+  {
+    "revision": "d499dc71f7eaf1f4bf25",
+    "url": "/drum-machine-react/static/js/runtime-main.47898ec7.js"
+  }
+]);
